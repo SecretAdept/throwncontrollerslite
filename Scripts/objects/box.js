41d30b3b145e3c31c32fee129dec317657b02d74
SecretAdept <68935611+SecretAdept@users.noreply.github.com>
2020-09-01 22:50:33 -0400

    Added questions and fixed formatting

--- Scripts/objects/box.js
+++ Scripts/objects/box.js
@@ -15,16 +15,25 @@ var objects;
 (function (objects) {
     var Box = /** @class */ (function (_super) {
         __extends(Box, _super);
-        function Box(id, x, y, color) {
+        function Box(id, x, y, color, text) {
             if (x === void 0) { x = 0; }
             if (y === void 0) { y = 0; }
             if (color === void 0) { color = "000"; }
+            if (text === void 0) { text = ""; }
             var _this = _super.call(this) || this;
             _this.num = id;
             _this.cursor = "pointer";
             _this.graphics.beginFill(color).drawRect(x, y, 150, 150);
             _this.on("mouseover", _this.mouseOver);
             _this.on("mouseout", _this.mouseOut);
+            _this.text = new objects.Label(text, "20pt", "Candara", "#000", x + 75, y + 75);
+            _this.text.lineWidth = 150;
+            _this.text.maxWidth = 150;
+            _this.text.textAlign = "center";
+            _this.text.textBaseline = "middle";
+            if (_this.text.text.split(" ").length > 2) {
+                _this.text.y = y + 55;
+            }
             return _this;
         }
         Box.prototype.mouseOver = function () {
@@ -32,8 +41,6 @@ var objects;
             this.y -= 5;
             this.scaleX *= 1.01;
             this.scaleY *= 1.01;
-            this.bgs = createjs.Sound.play("select");
-            this.bgs.volume = 0.5;
         };
         Box.prototype.mouseOut = function () {
             this.x += 5;
